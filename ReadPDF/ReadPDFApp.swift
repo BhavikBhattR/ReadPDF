308fa15a439a46b7e235365250a7c749a3454b2e
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-02-13 23:29:39 +0530

    Initial Commit

---
+++ ReadPDF/ReadPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadPDFApp.swift
+//  ReadPDF
+//
+//  Created by Bhavik Bhatt on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
